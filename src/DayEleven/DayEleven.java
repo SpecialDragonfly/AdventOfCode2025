719992baccf17a2b0d7c1620e793eea4ae807dcb
Dominic Orme <dominic.orme@tesco.com>
2025-12-11 20:57:08 +0000

    The Shortening

--- src/DayEleven/DayEleven.java
+++ src/DayEleven/DayEleven.java
@@ -14,14 +14,13 @@ public class DayEleven {
             nodes.put(parts[0], new Node(parts[0], outputs));
         }
         shortenNodes();
-        HashMap<String, PathResult> cache = new HashMap<>();
-        int result = followPath(cache, "svr", false, false, 0);
+        int result = followPath("svr", false, false, 0);
         System.out.println("Result: " + result);
     }
 
     private static void shortenNodes() {
         HashMap<String, String> nodesWithOnlyOneChild = new HashMap<>();
-        for (Map.Entry<String, Node> entry : DayEleven.nodes.entrySet()) {
+        for (Map.Entry<String, Node> entry : nodes.entrySet()) {
             if (entry.getKey().equals("fft") || entry.getKey().equals("dac")) {
                 continue;
             }
@@ -30,41 +29,28 @@ public class DayEleven {
             }
         }
         System.out.println("There were " + nodesWithOnlyOneChild.size() + " shortenings");
-        for (Map.Entry<String, Node> node : DayEleven.nodes.entrySet()) {
+        for (Map.Entry<String, Node> node : nodes.entrySet()) {
             for (Map.Entry<String, String> onlyChild : nodesWithOnlyOneChild.entrySet()) {
                 if (node.getValue().outputs().contains(onlyChild.getKey())) {
-                    DayEleven.nodes.get(node.getKey()).replaceValue(onlyChild.getKey(), onlyChild.getValue());
+                    nodes.get(node.getKey()).replaceValue(onlyChild.getKey(), onlyChild.getValue());
+                    nodes.remove(node.getKey());
                 }
             }
         }
     }
 
-    private static PathResult followPath(HashMap<String, PathResult> cache, String nodeId, boolean seenFFT, boolean seenDAC, int pathLength) {
+    private static int followPath(String nodeId, boolean seenFFT, boolean seenDAC, int pathLength) {
         if (nodeId.equals("out")) {
-            return new PathResult(1, seenFFT, seenDAC);
+            return seenFFT && seenDAC ? 1 : 0;
         }
         int result = 0;
-        if (seenFFT) {
-            if (cache.containsKey(nodeId) && cache.get(nodeId).seenDAC) {
-                System.out.println("Using cache");
-                return cache.get(nodeId);
-            }
-        }
 
         for (String node : nodes.get(nodeId).outputs()) {
-            result = followPath(cache, node, seenFFT || nodeId.equals("fft"), seenDAC || nodeId.equals("dac"), pathLength+1);
-        }
-        if (nodeId.equals("dac")) {
-            cache.put(nodeId, result);
-        }
-        if (nodes.get(nodeId).outputs().contains("dac")) {
-            cache.put(nodeId, result);
+            result += followPath(node, seenFFT || nodeId.equals("fft"), seenDAC || nodeId.equals("dac"), pathLength+1);
         }
         if (result > 0) {
             System.out.println("Path Length:" + pathLength + " = " + result);
         }
         return result;
     }
-
-    public record PathResult(int path, boolean seenFFT, boolean seenDAC){};
 }
